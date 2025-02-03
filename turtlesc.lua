c3d90fcfb2ec6c1c30c6d5b97997f470a5c73036
Fjyetto <89774326+Fjyetto@users.noreply.github.com>
2025-02-03 21:10:26 +0100

    Update turtlesc.lua

--- turtlesc.lua
+++ turtlesc.lua
@@ -3,7 +3,18 @@ local sfun = {[true]=turtle.turnRight,[false]=turtle.turnLeft}
 for z=1,16 do
 	for x=1,15 do
 		turtle.dig()
-		turtle.forward()
+		local l,r = turtle.forward()
+		if not l and r=="Out of fuel" then
+		
+			for i = 1, 16 do -- loop through the slots
+			  turtle.select(i)
+			  if turtle.refuel(0) then
+				local halfStack = math.ceil(turtle.getItemCount(i)/2)
+				turtle.refuel(halfStack)
+			  end
+			end
+			
+		end
 	end
 	side = not side
 	
